9d63f72fc967c088a8caf001054e1f2da4f437fc
Nicolas <36771870+nicolasFsoares@users.noreply.github.com>
2023-06-28 12:06:10 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var hash = window.location.hash;
+if (hash && hash !== "") {
+    var target = hash.substring(1);
+    var divAlvo = document.getElementById(target);
+    divAlvo.scrollIntoView();
+
+}
+
+function goToPage(page){
+    window.location = page+'.html';
+}  
+
+window.addEventListener('load', function (event) {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150; 
+      reveals[i].classList.add("active");
+    }
+
+});
+window.addEventListener("scroll", reveal);
